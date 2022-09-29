e9b004bd47f447ba2654c5361779e9669d399ee3
kvnal <72142540+kvnal@users.noreply.github.com>
2022-09-29 11:11:24 +0530

    module : calculation - insights

--- src/calculation.js
+++ src/calculation.js
@@ -1,5 +1,5 @@
 class Calculate{
-    progressTrackerMetrics(issues, user = false){
+    progressTrackerMetrics(issues){
         //user = false > progress tracker for all issues > it will calc. assigned & unassigned issues.
         const totalIssues = issues.length;
         
@@ -11,30 +11,65 @@ class Calculate{
             done : 0
         }
 
+        let insights = new Map();
 
         issues.forEach(issue => {
-            if(!user && issue.fields.assignee == null){
+            
+            const statusName = issue.fields.status.statusCategory.name;
+
+            if(statusName == "To Do") metrics.todo++;
+            if(statusName == "Done") metrics.done++;
+            if(issue.fields.assignee == null){
                 metrics.unassigned++;
             }
+            else{
+                //insights
+                let assignee = issue.fields.assignee;
 
-            if(issue.fields.status.name == "To Do") metrics.todo++;
-            if(issue.fields.status.name == "Done") metrics.done++;
+                if(insights.has(assignee.accountId)){
+                    let accountIdData = insights.get(assignee.accountId);
+                    
+                    accountIdData.progress.total++;
+                    if(statusName == "To Do") accountIdData.progress.todo++;
+                    if(statusName == "Done") accountIdData.progress.done++;
+
+                    //update data
+                    insights.set(assignee.accountId , accountIdData );
+                }
+                else{
+                    //new hashmap
+
+                    const defaultInsightsMetrics = {
+                        accountId : assignee.accountId,
+                        displayName : assignee.displayName,
+                        avatarUrl : assignee.avatarUrls['32x32'],
+                        progress : {
+                            total : 1,
+                            todo : statusName == "To Do" ? 1 : 0,
+                            done : statusName == "Done" ? 1 : 0
+                        }
+                    }
+
+                    insights.set(assignee.accountId , defaultInsightsMetrics);
+
+                }
+            }
+
         });
         
         metrics.progress = totalIssues - metrics.done - metrics.todo;
-        if(!user){
-            metrics['assigned'] = totalIssues - metrics.unassigned;
-        }
+        metrics['assigned'] = totalIssues - metrics.unassigned;
 
+        console.log(insights);
         return metrics;
     }
 }
 
 
-/* // Test>> 
+ // Test>> 
 
-const obj = new Calculate();
-console.log(obj.progressTrackerMetrics(obj.test));
-*/
+// const obj = new Calculate();
+// console.log(obj.progressTrackerMetrics(obj.test));
+
 
 export default Calculate;
