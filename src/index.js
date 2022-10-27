ac22505ab95c0f75fe986c806c44154ec58bb2fe
kvnal <72142540+kvnal@users.noreply.github.com>
2022-10-27 23:14:00 +0530

    random announcement added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import Resolver from '@forge/resolver';
-import {storage} from '@forge/api'
+import { storage } from '@forge/api';
 
 const resolver = new Resolver();
 
@@ -7,119 +7,119 @@ const resolver = new Resolver();
 import Config from './config';
 import API from './api';
 
-resolver.define('getText',  async (req) => {
- 
-    return "🔴 Live data"; 
+resolver.define('getText', async (req) => {
+
+    return "🔴 Live data";
 });
 
 //home
-resolver.define("getProgressMetrics" , async (req)=>{
+resolver.define("getProgressMetrics", async (req) => {
     // await config.resetAllStoredData();
     const projectId = req.context.extension.project.id;
 
     const config = new Config(projectId);
-    
-    
+
+
     console.log(`GET - progress metrics for ${projectId}`);
     const checkStatus = await config.checkAndUpdate();
-    if(checkStatus==1){
+    if (checkStatus == 1) {
         const customApi = new API(projectId);
         const data = await customApi.getMetrics();
-        return data;   
+        return data;
     }
     return checkStatus;
-})
+});
 
-resolver.define("getIssueData" , async (req)=>{
+resolver.define("getIssueData", async (req) => {
     console.log("GET - issue & recommendation data ");
 
     const projectId = req.context.extension.project.id;
 
-    const customApi = new API(projectId)
+    const customApi = new API(projectId);
 
-    const {issueId} = req.payload;
+    const { issueId } = req.payload;
     const response = await customApi.getIssueAndRecommendatation(issueId);
     return response;
-})
+});
 
-resolver.define("setAssignee" , async (req)=>{
+resolver.define("setAssignee", async (req) => {
     console.log("SET - assignee ");
 
     const projectId = req.context.extension.project.id;
     const customApi = new API(projectId);
 
-    const {assigneeId} = req.payload;
-    const {issueId} = req.payload;
-    const response = await customApi.setAssignee(issueId,assigneeId);
-    console.log('Issue',JSON.stringify(response));
+    const { assigneeId } = req.payload;
+    const { issueId } = req.payload;
+    const response = await customApi.setAssignee(issueId, assigneeId);
+    console.log('Issue', JSON.stringify(response));
     // const response = await customApi.getMetrics();
     return response;
-})
+});
 
-resolver.define("getUserData" , async (req)=>{
+resolver.define("getUserData", async (req) => {
     console.log("GET - stored user's data ");
 
     const projectId = req.context.extension.project.id;
 
-    const userData = await storage.get('userData'+`_${projectId}`);
+    const userData = await storage.get('userData' + `_${projectId}`);
     return userData;
-})
+});
 
-resolver.define("getStorypoint" , async (req)=>{
+resolver.define("getStorypoint", async (req) => {
     console.log("GET - stored storypoint");
     const projectId = req.context.extension.project.id;
-    let sp = await storage.get('sprintStorypoint'+`_${projectId}`);
-    let defaultSP = await storage.get('defaultStorypoint'+`_${projectId}`)
-    let sprintID = await storage.get('activeSprintId'+`_${projectId}`)
+    let sp = await storage.get('sprintStorypoint' + `_${projectId}`);
+    let defaultSP = await storage.get('defaultStorypoint' + `_${projectId}`);
+    let sprintID = await storage.get('activeSprintId' + `_${projectId}`);
 
     const customApi = new API(projectId);
     let boardId = await customApi.getCurrentBoardId();
 
     const result = {
-        projectId : projectId,
-        boardId : boardId,
-        sp : sp,
-        sprintSPLimit : defaultSP,
-        sprintID : sprintID
-    }
-    return JSON.stringify(result); 
-})
+        projectId: projectId,
+        boardId: boardId,
+        sp: sp,
+        sprintSPLimit: defaultSP,
+        sprintID: sprintID
+    };
+    return JSON.stringify(result);
+});
 
-resolver.define('setDefaultStorypoint', async (req)=>{
+resolver.define('setDefaultStorypoint', async (req) => {
 
     console.log("SET - default Story Point");
 
     const projectId = req.context.extension.project.id;
 
     const config = new Config(projectId);
-    
-    const {value} = req.payload;
+
+    const { value } = req.payload;
     config.updateDefaultStorypoint(value);
-    return {message : "default storypoint updated!"};
-})
+    return { message: "default storypoint updated!" };
+});
 
 //standup
-resolver.define('setStandupDetails', async (req)=>{
+resolver.define('setStandupDetails', async (req) => {
 
-    console.log(`SET - standup Details` );
+    console.log(`SET - standup Details`);
     const projectId = req.context.extension.project.id;
 
-    try{
-        
+    try {
+
         const issueId = req.context.extension.issue.id;
         const key = req.context.extension.issue.key;
-        const {accountId} = req.context;
-        
+        const { accountId } = req.context;
+
         const customApi = new API(projectId);
-        const result =await customApi.setStandupDetails(issueId, key, accountId, req.payload);
+        const result = await customApi.setStandupDetails(issueId, key, accountId, req.payload);
         return result;
     }
-    catch(e){
-        return {message : "submit this from issue activity"}
+    catch (e) {
+        return { message: "submit this from issue activity" };
     }
-})
+});
 
-resolver.define('getStandupDetails', async (req)=>{
+resolver.define('getStandupDetails', async (req) => {
 
     console.log("GET - Standup Details");
 
@@ -129,73 +129,102 @@ resolver.define('getStandupDetails', async (req)=>{
 
     const result = await customApi.getStandupDetails();
     return result;
-})
+});
 
 
-resolver.define("setStandupDetailsNotes", async (req)=>{
+resolver.define("setStandupDetailsNotes", async (req) => {
 
-    const {standupId} = req.payload;
-    const {notes} = req.payload;
+    const { standupId } = req.payload;
+    const { notes } = req.payload;
 
-    console.log(`SET - standup Details Notes for ${standupId}`)
+    console.log(`SET - standup Details Notes for ${standupId}`);
 
     const projectId = req.context.extension.project.id;
-    
+
     const customApi = new API(projectId);
 
     const response = await customApi.setStandupDetailsNotes(standupId, notes);
     return response;
-})
+});
 
 //dev api
-resolver.define("deleteStorageData", async (req)=>{
+resolver.define("deleteStorageData", async (req) => {
 
     const projectId = req.context.extension.project.id;
 
     const customApi = new API(projectId);
 
-    console.log(`! Delete - storageDate for ${req.payload.key}`)
+    console.log(`! Delete - storageDate for ${req.payload.key}`);
     const response = await customApi.deleteStorageData(req.payload.key);
     return response;
-})
+});
 
-resolver.define('setStandupDetailsTimelimit' , async (req)=>{
+resolver.define('setStandupDetailsTimelimit', async (req) => {
     const projectId = req.context.extension.project.id;
 
     console.log(`SET - Standup Time for ${projectId}`);
     const customApi = new API(projectId);
     await customApi.setStandupTimelimit(req.payload.timeInMinutes);
-    return {message : "Standup Timelimit updated successfully"};
-})
+    return { message: "Standup Timelimit updated successfully" };
+});
 
-resolver.define("getAnnouncement", async(req)=>{
-    const announcement = {
-        visible : true,
-        title:"hello world",
-        message : "Checkout out the scrumzilla's walk-through video!",
-        buttonText : "Link",
-        link : "https://youtube.com"
-    }
-
-    return announcement;
-})
-
-resolver.define("getProjectSettingData" , async (req)=>{
-    console.log("GET - project setting data");
+resolver.define("getAnnouncement", async (req) => {
+    const announcement = [
+        {
+            title: "Hey!!",
+            message: "Checkout out a quick walk-through video!",
+            buttonText: "Watch now",
+        },
+        {
+            title: "Hello, world!",
+            message: "Watch a quick app set-up video!",
+            buttonText: "Video",
+        },
+        {
+            title: "Cat video",
+            message: "wanna watch a funny cat video?",
+            buttonText: "yes i want to watch!",
+        },
+        {
+            title: "Important",
+            message: "do not click the below button",
+            buttonText: "button",
+        },
+        {
+            title: "Help please",
+            message: "what happens if i click this button🤔?",
+            buttonText: "lemme check!",
+        },
+        {
+            title: "Tutorial",
+            message: "click on the below link to watch scrumzilla quick walk-through video tutorial.",
+            buttonText: "watch now",
+        }
+    ];
     
+    let index = Math.floor(Math.random() * (announcement.length + 1));
+    let randomAnnouncement = announcement[index];
+    randomAnnouncement['visible'] = false;
+    randomAnnouncement['link'] = "https://youtube.com";
+
+    return randomAnnouncement;
+});
+
+resolver.define("getProjectSettingData", async (req) => {
+    console.log("GET - project setting data");
+
     const projectId = req.context.extension.project.id;
 
-    const timelimit = await storage.get("standupTimelimit" + `_${projectId}`)
-    const storyPoint = await storage.get("defaultStorypoint" + `_${projectId}`)
+    const timelimit = await storage.get("standupTimelimit" + `_${projectId}`);
+    const storyPoint = await storage.get("defaultStorypoint" + `_${projectId}`);
 
-    const setting ={
-        timelimit : timelimit? timelimit : 12,
-        defaultStoryPoint : storyPoint
-    }
+    const setting = {
+        timelimit: timelimit ? timelimit : 12,
+        defaultStoryPoint: storyPoint
+    };
     return setting;
-})
+});
 
 
 export const handler = resolver.getDefinitions();
 
- 
